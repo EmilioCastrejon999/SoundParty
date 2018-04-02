13495c07bdc96472a14f3af19cca0087bb509627
Emilio Castrejon <emiliofermincg@hotmail.com>
2018-04-02 11:50:57 -0500

    Initial Commit

---
+++ SoundParty/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SoundParty
+//
+//  Created by Emilio Castrejon Guerrero on 4/2/18.
+//  Copyright © 2018 Opindes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SoundParty/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SoundParty
+//
+//  Created by Emilio Castrejon Guerrero on 4/2/18.
+//  Copyright © 2018 Opindes. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
